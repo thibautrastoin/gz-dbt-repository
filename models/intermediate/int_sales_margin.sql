a861d2aba11123da6dfba7b4710e4422b7d5e23d
Thibaut Rastoin <thibautrastoin@gmail.com>
2024-02-06 15:51:49 +0000

    margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,16 +4,15 @@ WITH sales_product AS
 sales.products_id,
 sales.date, 
 sales.orders_id, 
-sales.revenue,
-sales.quantity, 
-product.purchase_price, 
+CAST (sales.revenue as FLOAT64) as revenue,
+CAST (sales.quantity as FLOAT64) as quantity, 
+CAST (product.purchase_price as FLOAT64) as purchase_price, 
 FROM {{ref('stg_raw__sales')}} as sales 
 LEFT JOIN {{ref('stg_raw__product')}} as product 
 USING (products_id)
 )
 
-SELECT 
-*,
-FROM sales_product 
-
-
+SELECT *,
+ROUND (revenue - purchase_price,2) as margin,
+ROUND (quantity*purchase_price,2) as purchase_cost,
+FROM sales_product
