99669df7a830146200860967c74a20cefd4cf536
Thibaut Rastoin <thibautrastoin@gmail.com>
2024-02-07 10:56:08 +0000

    j

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,3 +1,5 @@
+{{ config(materialized = 'table') }}
+
 SELECT
 date,
 
@@ -5,6 +7,7 @@ ROUND(SUM(purchase_price),2) as purchase_cost,
 COUNT(orders_id) as orders,
 ROUND(SUM (op_margin),2) as op_margin,
 SUM (quantity) as qty,
+ROUND(SUM(revenue)/COUNT(orders_id),2) as average_basket,
 FROM {{ref('int_orders_operational')}} 
 GROUP BY date
 ORDER BY date DESC
